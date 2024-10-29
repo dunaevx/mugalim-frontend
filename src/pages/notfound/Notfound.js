c2ffb33716cdb4ec1946b98ece65774703415d0b
dunaevx <lol69056@gmail.com>
2024-10-30 00:09:11 +0600

    finished error page

--- src/pages/notfound/Notfound.js
+++ src/pages/notfound/Notfound.js
@@ -1,9 +1,17 @@
 import React from 'react'
+
 import './Notfound.css'
+import icon404 from '../../assets/icon404.svg'
 
 function Notfound() {
   return (
-    <div>Notfound</div>
+    <div className='notfound'>
+      <div className='notfound__content'>
+        <img alt="icon404" src={icon404} />
+        <h1>404</h1>
+        <p>Ой, что-то пошло не так...</p>
+      </div>
+    </div>
   )
 }
 

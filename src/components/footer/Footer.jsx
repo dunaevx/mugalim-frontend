d9268a7948a3a94e456c1099fe8b4481383caf4a
dunaevx <lol69056@gmail.com>
2024-11-08 20:59:28 +0600

    fixed bag

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,14 +5,14 @@ import logo from '../../assets/Logo.svg'
 
 const Footer = () => {
   return (
-    <footer class="footer">
-  <div class="footer-content">
-    <div class="footer-logo">
+    <footer className="footer">
+  <div className="footer-content">
+    <div className="footer-logo">
       <img src={logo} alt="logo" />
       <p>Платформа для создания тестов и поддержки преподавателей.</p>
     </div>
 
-    <div class="footer-links">
+    <div className="footer-links">
       <h3>Навигация</h3>
       <ul>
         <li><a href="/generate-test">Создать тест</a></li>
@@ -22,7 +22,7 @@ const Footer = () => {
       </ul>
     </div>
 
-    <div class="footer-contact">
+    <div className="footer-contact">
       <h3>Контакты</h3>
       <p>Email: support@mugalim.com</p>
       <p>Телефон: +9 (996) 777-777-777</p>
@@ -30,7 +30,7 @@ const Footer = () => {
     </div>
   </div>
 
-  <div class="footer-bottom">
+  <div className="footer-bottom">
     <p>&copy; 2024 Mugalim. Все права защищены.</p>
     <p><a href="/privacy-policy">Политика конфиденциальности</a> | <a href="/terms-of-service">Условия использования</a></p>
   </div>
